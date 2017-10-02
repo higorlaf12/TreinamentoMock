44bdbd48a8dbde2697bfce0baf373f3e5f6c4bce
higorlaf12 <higor.freitas@neppoadm.com.br>
2017-10-02 14:26:52 -0300

    add refector DomainBuyController

--- treinamentoMock/src/main/java/com/treinamento/Mock/treinamentoMock/controller/DomainBuyController.java
+++ treinamentoMock/src/main/java/com/treinamento/Mock/treinamentoMock/controller/DomainBuyController.java
@@ -34,10 +34,10 @@ public class DomainBuyController {
     public ResponseEntity<Object> findOneDomainBuy(@PathVariable(name = "id") Integer id){
         try {
             DomainBuy search = domainBuyServer.findOneDomainBuy(id);
-            if (search.getId() != null){
-                return new ResponseEntity<>(search,HttpStatus.OK);
+            if (search == null){
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
             }
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(search,HttpStatus.OK);
         }catch (Exception e){
             e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
